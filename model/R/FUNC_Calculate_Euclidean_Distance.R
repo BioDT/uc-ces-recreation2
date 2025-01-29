220212ade313ba6da32a2442946925c54a7c1970
jmarshrossney <17361029+jmarshrossney@users.noreply.github.com>
2025-01-29 10:25:43 +0000

    use lintr and stylr; tidy up 'euclidean distance'

--- model/R/FUNC_Calculate_Euclidean_Distance.R
+++ model/R/FUNC_Calculate_Euclidean_Distance.R
@@ -1,34 +1,24 @@
-# input is a list of rasters
-# output is also a list of rasters
+#' Calculate Euclidean Distance
+#'
+#' This functions computes the Euclidean distance ???
+#'
+#' @param proximity_rasters A list of rasters
+#' @param alpha ?
+#' @param kappa ?
+#' @return A list of rasters
+#' @export
 calculate_euclidean_distance <- function(proximity_rasters, alpha = 0.01101, kappa = 5) {
     # Create an empty list to store the output rasters
     euc_dist_rasters <- list()
 
     # Loop over the proximity rasters
     for (score in names(proximity_rasters)) {
-        # Perform the calculation
-        euc_dist_rasters[[score]] <- ((kappa + 1)/(kappa + exp(proximity_rasters[[score]] * alpha))) *
-            as.numeric(gsub("^score(\\d+)$", "\\1", score))
+        euc_dist_rasters[[score]] <- ((kappa + 1) / (kappa + exp(proximity_rasters[[score]] * alpha))) *
+            as.numeric(score)
     }
 
+    terra_rasters <- terra::rast(euc_dist_rasters)
+
     # Return the output rasters
-    return(terra::rast(euc_dist_rasters))
+    return(terra_rasters)
 }
-
-
-
-# # input is a list of rasters
-# # output is also a list of rasters
-# calculate_euclidean_distance <- function(proximity_rasters, alpha = 0.01101, kappa = 5) {
-#   # Create an empty list to store the output rasters
-#   euc_dist_rasters <- list()
-#   
-#   # Loop over the proximity rasters
-#   for (score in names(proximity_rasters)) {
-#     # Perform the calculation
-#     euc_dist_rasters[[score]] <- ((kappa + 1) / (kappa + exp(proximity_rasters[[score]] * alpha))) * as.numeric(gsub('^score(\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\d+)$', '\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\1', score))
-#   }
-#   
-#   # Return the output rasters
-#   return(euc_dist_rasters)
-# }
---
+++ model/pre-commit.R
@@ -0,0 +1,12 @@
+style <- styler::tidyverse_style(
+    indent_by = 4L,
+    start_comments_with_one_space = TRUE
+)
+
+# TODO: apply to the entire package
+styler::style_file(
+    "R/FUNC_Calculate_Euclidean_Distance.R",
+    transformers = style
+)
+
+lintr::lint("R/FUNC_Calculate_Euclidean_Distance.R")
