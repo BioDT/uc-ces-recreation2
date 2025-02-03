6117e09328a3a9b9a44d5a52430b0ae6d3b8597f
jmarshrossney <17361029+jmarshrossney@users.noreply.github.com>
2025-02-03 12:01:53 +0000

    add layer selection

--- shiny_app/app.R
+++ shiny_app/app.R
@@ -1,4 +1,5 @@
 library(shiny)
+library(leaflet)
 
 devtools::load_all("../model")
 
@@ -30,55 +31,92 @@ create_sliders <- function(component) {
     )
 }
 
-plot_potential <- function(persona_) {
-    rasters <- model::compute_potential(persona_, raster_dir = "data/one_hot/")
-    terra::plot(rasters[["Water"]])
-}
+palette <- colorNumeric(
+    palette = "viridis",
+    domain = c(0, 1),
+    na.color = "transparent"
+)
 
 ui <- fluidPage(
+    tags$head(
+        tags$style(HTML("html, body {height: 100%;} #map {height: 80vh !important;}"))
+    ),
     titlePanel("BioDT - Scotland Recreational Model"),
     fluidRow(
         column(
             width = 6,
             tabsetPanel(
-                tabPanel(
-                    "SLSRA",
-                    create_sliders("SLSRA")
-                ),
-                tabPanel(
-                    "FIPS_N",
-                    create_sliders("FIPS_N")
-                ),
-                tabPanel(
-                    "FIPS_I",
-                    create_sliders("FIPS_I")
-                ),
-                tabPanel(
-                    "Water",
-                    create_sliders("Water")
-                )
+                tabPanel("SLSRA", create_sliders("SLSRA")),
+                tabPanel("FIPS_N", create_sliders("FIPS_N")),
+                tabPanel("FIPS_I", create_sliders("FIPS_I")),
+                tabPanel("Water", create_sliders("Water"))
             )
         ),
         column(
             width = 6,
-            actionButton("computeButton", "Compute"),
-            plotOutput("potential_heatmap")
+            actionButton("updateButton", "Update Map"),
+            radioButtons(
+                "layerSelect",
+                "Select Layer",
+                choices = list(
+                    "SLSRA" = 1,
+                    "FIPS_N" = 2,
+                    "FIPS_I" = 3,
+                    "Water" = 4,
+                    "Recreational Potential" = 5
+                ),
+                selected = 5,
+                inline = TRUE
+            ),
+            leafletOutput("map")
         )
     )
 )
 
-server <- function(input, output) {
-    # Create reactive expression to store updated persona values
-    persona_reactive <- eventReactive(input$computeButton, {
-        sapply(
+server <- function(input, output, session) {
+    # Initialize Leaflet map
+    output$map <- renderLeaflet({
+        leaflet() |>
+            setView(lng = -4.2026, lat = 56.4907, zoom = 7) |>
+            addTiles() |>
+            addLegend(pal = palette, values = c(0, 1), title = "Values")
+    })
+
+    # Reactive variable for caching layers
+    reactiveLayers <- reactiveVal()
+
+    # Grabs cached layers and updates map with current layer selection
+    update_map <- function() {
+        req(reactiveLayers())
+
+        layers <- reactiveLayers()
+        curr_layer <- layers[[as.numeric(input$layerSelect)]]
+
+        leafletProxy("map") |>
+            clearImages() |>
+            addRasterImage(curr_layer, colors = palette, opacity = 0.8)
+    }
+
+
+    # Recompute raster when update button is clicked
+    observeEvent(input$updateButton, {
+        persona_ <- sapply(
             names(persona),
             function(layer_name) input[[layer_name]],
             USE.NAMES = TRUE
         )
+        layers <- model::compute_potential(persona_, raster_dir = "data/one_hot/") |>
+            model::rescale_to_unit_interval() # TODO: do this in compute_potential
+
+        # Update reactiveLayers with new raster
+        reactiveLayers(layers)
+
+        update_map()
     })
 
-    output$potential_heatmap <- renderPlot({
-        plot_potential(persona_reactive())
+    # Update map using cached values when layer selection changes
+    observeEvent(input$layerSelect, {
+        update_map()
     })
 }
 
